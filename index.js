b5b537979ebc3b11c1d35de3d79e20a180edc187
Nourioo <zahra.nouri.sigriswil@gmail.com>
2025-09-01 17:59:31 +0330

    u[

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-function generatePoem (event){
-event.preventDefault();
-new Typewriter('#poem', {
-  strings: ["La tombe dit à la rose"],
-  autoStart: true,
-  delay: 1,
-  cursor:"",
-});
+function displayPoem(response){
+  console.log("Poem generated");
+  new TypeWriter("#poem", {
+    string: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
 }
+ 
+function generatePoem(event){
+  event.preventDefault();
 
+  let instuctionInput= document.querySelector("#user-instructions");
+  let apiKey ="540f410t39afca518e877dc5abodd75b";
+  let prompt= `Uder instructions: Generate a French poem about ${instuctionInput.value} `;
+  let context= "You are a romantic Poem expert and you love to write short poems, your mission is to generate a 4 line poem in HTML format. make sure to follow the user instructions";
+  let apiUrl= `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log (`context: ${context}`);
+
+  axios.get(apiUrl).then(displayPoem);
+
+}
 let poemFormElement= document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
